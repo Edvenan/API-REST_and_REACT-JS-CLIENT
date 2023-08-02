8eb431bb557d64e69a0b4d2d1c87585db082b287
Edvenan <e_vendrell@yahoo.com>
2023-08-02 12:05:39 +0200

    Method created to create a new Game for a particular User

--- Dices/app/Models/Game.php
+++ Dices/app/Models/Game.php
@@ -10,7 +10,17 @@ class Game extends Model
 {
     use HasFactory;
 
-    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'dice_1',
+        'dice_2',
+        'result',
+    ];    
     
     
     
