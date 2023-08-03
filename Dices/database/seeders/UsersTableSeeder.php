5878bbf17077413d044738ad4bedf75d5d0e2f7c
Edvenan <e_vendrell@yahoo.com>
2023-08-03 19:18:09 +0200

    Improved Seeders and Factories

--- Dices/database/seeders/UsersTableSeeder.php
+++ Dices/database/seeders/UsersTableSeeder.php
@@ -4,8 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Game;
 use App\Models\User;
-use Database\Factories\RoleFactory;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use \App\Models\Role;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
@@ -16,23 +15,23 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
+        // Create Admin user
         User::create(['name' => 'SuperUser', 
-                                'email' => 'admin@admin.com', 
-                                'email_verified_at' => now(),
-                                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                                'remember_token' => Str::random(10)])
+                    'email' => 'admin@admin.com', 
+                    'email_verified_at' => now(),
+                    'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                    'remember_token' => Str::random(10)])
             ->each(function($user) {
                 $user->role()->create(['role' => 'admin']);
         });
 
-        User::factory(10)->create()
-        ->each(function($user) {
-            $user->role()->save(\App\Models\Role::factory()->make());
-        });
 
-        Game::factory(100)->create();
+        // Create Players and Games for each Player
+        User::factory(10)
+            ->has(Role::factory())
+            ->has(Game::factory(10))
+            ->create();
 
     }
 }
