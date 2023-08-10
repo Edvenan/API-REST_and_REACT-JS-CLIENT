d35537340ac7d322dd9a2ec595f1fa299e16d69f
Edvenan <e_vendrell@yahoo.com>
2023-08-10 17:08:21 +0200

    Test suite update for authorization,  common helper functions and properties moved to BaseTestCase and minor cosmetic changes in api.php

--- Dices/tests/Feature/AuthorizationTest.php
+++ Dices/tests/Feature/AuthorizationTest.php
@@ -2,31 +2,14 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
-use Database\Seeders\UsersTableSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Testing\Fluent\AssertableJson;
-use Tests\TestCase;
+use Tests\BaseTestCase;
 
-class AuthorizationTest extends TestCase
+class AuthorizationTest extends BaseTestCase
 {
     use RefreshDatabase;
 
-    /**
-     * Test case suite to test User Registration, Login and Logout
-     */
-
-    const URL = "http://localhost:8000/api/v1";
-    
-    /**
-     * Set up Passport OAuth Personal Access Clients
-    */
-    public function setUp(): void
-    {
-    parent::setUp();
-    $this->artisan('passport:install');
-    }
+    // Test case suite to test User Registration, Login and Logout
 
     /**
      * Test cases to test user creation/registration [POST/players]
@@ -41,10 +24,10 @@ class AuthorizationTest extends TestCase
             'c_password' => 'password',
         ]);
 
-        $response->assertStatus(201);
+        $response->dump()->assertStatus(201)->assertSee('created_at');
     }
     /** @test */
-    public function a_user_registers_unsuccessfully(): void
+    public function a_user_registers_with_different_confirmed_password(): void
     {
         $response = $this->post(self::URL .'/players', [
             'name' => 'Mr. Fullstack',
@@ -55,6 +38,77 @@ class AuthorizationTest extends TestCase
 
         $response->assertBadRequest();
     }
+    /** @test */
+    public function a_user_registers_without_name_and_gets_anonymous(): void
+    {
+        $response = $this->post(self::URL .'/players', [
+            'email' => 'fullstack@sample.com',
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+
+        $response->dump()->assertSuccessful()->assertJsonPath('data.name', 'Anonymous');
+    }
+    /** @test */
+    public function two_users_register_with_blank_names_and_both_get_anonymous(): void
+    {
+        $response = $this->post(self::URL .'/players', [
+            'name' => '',
+            'email' => 'fullstack@sample.com',
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+        $response->dump()->assertSuccessful()->assertJsonPath('data.name', 'Anonymous');
+
+        $response = $this->post(self::URL .'/players', [
+            'email' => 'php@sample.com',
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+
+        $response->dump()->assertSuccessful()->assertJsonPath('data.name', 'Anonymous');
+    }
+    /** @test */
+    public function a_user_registers_with_same_name_as_another_user(): void
+    {
+        $response = $this->post(self::URL .'/players', [
+            'name' => 'Mr Dummy Name',
+            'email' => 'fullstack@sample.com',
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+        $response->assertSuccessful();
+
+        $response = $this->post(self::URL .'/players', [
+            'name' => 'Mr Dummy Name',
+            'email' => 'another_email@sample.com',
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+
+        $response->dump()->assertBadRequest()->assertSee('The name has already been taken.');
+    }
+    /** @test */
+    public function a_user_registers_with_same_email_as_another_user(): void
+    {
+        $response = $this->post(self::URL .'/players', [
+            'name' => 'Mr Dummy Name',
+            'email' => 'fullstack@sample.com',
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+        $response->assertSuccessful();
+
+        $response = $this->post(self::URL .'/players', [
+            'name' => 'Mr Another Dummy Name',
+            'email' => 'fullstack@sample.com',
+            'password' => 'password',
+            'c_password' => 'password',
+        ]);
+
+        $response->dump()->assertBadRequest()->assertSee('The email has already been taken.');
+    }
+
     /**
      * Test case to test user login [POST/login]
     */
@@ -62,29 +116,25 @@ class AuthorizationTest extends TestCase
     public function a_user_logs_in_successfully(): void
     {
         [$user, $scope] = $this->createDummyUser();
+        $credentials = ['email' => $user->email, 'password' => 'password'];
 
-        $response = $this->post(self::URL.'/login', [
-            'email' => $user->email,
-            'password' => 'password'
-        ]);
+        $response = $this->post(self::URL.'/login', $credentials);
 
         $response->assertStatus(201)
                  ->assertJsonPath('data.token', fn (string $token) => strlen($token) >= 3);
         $this->assertArrayHasKey('token', $response['data']);
-
+        $this->assertCredentials( $credentials, $guard = 'api');
     }
     /** @test */
-    public function a_user_logs_in_unsuccessfully(): void
+    public function a_user_logs_in_with_wrong_passowrd(): void
     {
         [$user, $scope] = $this->createDummyUser();
+        $credentials = ['email' => $user->email, 'password' => 'wrong_password'];
 
-        $response = $this->post(self::URL.'/login', [
-            'email' => $user->email,
-            'password' => 'wrong_password'
-        ]);
+        $response = $this->post(self::URL.'/login', $credentials);
 
         $response->assertUnauthorized();
-
+        $this->assertInvalidCredentials($credentials, $guard = 'api');
     }
     /**
      * Test case to test user logout [POST/logout]
@@ -101,31 +151,17 @@ class AuthorizationTest extends TestCase
         $response->assertStatus(201);
     }
     /** @test */
-    public function a_user_logs_out_unsuccessfully(): void
+    public function a_user_logs_out_with_invalid_token(): void
     {
         [$user, $scope] = $this->createDummyUser();
         
         // Run a specific seeder...
         //$this->seed(UsersTableSeeder::class);
-        //$response->dump();
 
         $response = $this->withHeaders([
             'Authorization' => 'Bearer '.'wrong.token.',
         ])->post(self::URL.'/logout');
 
         $response->assertRedirect();
-    }
-
-    //---------------------------------------------------------//
-
-    /**
-     * Helper function that creates a test user
-     */
-    public function createDummyUser()
-    {
-        $user = User::factory()->create();
-        $user->role()->create(['role' => fake()->randomElement(['admin', 'player'])]);
-        $scope = $user->role()->first()->role;
-        return [$user, $scope];
     }
 }
