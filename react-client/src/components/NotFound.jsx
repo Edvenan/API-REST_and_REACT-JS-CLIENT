5f3b04d8ebf18bac543f09f3e6ce5248e9531eae
Edvenan <e_vendrell@yahoo.com>
2023-09-07 15:21:02 +0200

    Feature/NotFound _404 component - creation

---
+++ react-client/src/components/NotFound.jsx
@@ -0,0 +1,16 @@
+
+
+export default function NotFound() {
+
+    return (
+
+            <div className='flex flex-grow'>
+                <div className="flex flex-col w-full text-yellow-300 text-center justify-center">
+                    <p>404 - Page Not Found</p>
+                    <p>Oops! The page you're looking for does not exist</p>
+                    <br></br>
+                    <p>Click on the top-left dices icon to go back to the home page</p>
+                </div>
+            </div>
+    );
+}
