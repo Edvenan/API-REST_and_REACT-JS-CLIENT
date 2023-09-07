7166b1e52f9cd67687d8d43a7a21fc197433bebf
Edvenan <e_vendrell@yahoo.com>
2023-09-07 14:59:23 +0200

    Routes and  Context

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import HomePage from './components/HomePage';
+import { AuthProvider} from './services/AuthContext';
+import { AppProvider } from "./services/AppContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ react-client/src/services/AppContext.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+function AppProvider(props) {
+    const [state, setState] = useState({
+
+    })
+    return (
+        <AppContext.Provider value={[state, setState]}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export {AppProvider};
+export default AppContext;
